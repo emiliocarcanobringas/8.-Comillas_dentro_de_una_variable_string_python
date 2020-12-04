fe52523c5a03e5c36d6e8793430288ddce7bc3ae
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:58:16 -0600

    Add files via upload

---
+++ 8.-Comillas_dentro_de_una_variable_string_python.py
@@ -0,0 +1,9 @@
+# Este programa muestra como utilizar las comillas dentro de una variable string
+print("Este programa muestra como utilizar las comillas dentro de una variable string")
+cadena = 'Estoy "matando" el tiempo'
+print(cadena)
+print("El tipo de varibale es: ")
+print(type(cadena))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
